5e4cdb109deae081b6ad747883a50a74f330c9e6
Zach Berta-Thompson <zkbt@mit.edu>
2019-02-07 06:53:09 -0700

    add solarsytemdata

---
+++ astr2600/solarsystem.py
@@ -0,0 +1,12 @@
+# some basic data about the solar system, as lists
+
+names = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+masses = [3.3e+23, 4.87e+24, 5.97e+24, 6.42e+23, 1.9e+27, 5.68e+26, 8.68e+25, 1.02e+26]
+radii = [2440000.0,
+         6052000.0,
+         6378000.0,
+         3397000.0,
+         71492000.0,
+         60268000.0,
+         25559000.0,
+         24766000.0]
